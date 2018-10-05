f919825b211f729980cb0bfbb8167739c6328827
jtPox <me@jtpox.com>
2018-10-05 17:43:26 +0800

    Added viewing of visitor log and day range in stats.

--- src/components/widgets/Stats.vue
+++ src/components/widgets/Stats.vue
@@ -9,29 +9,71 @@
       title="Statistics" 
       size="lg" 
       hide-footer>
-      <div class="d-block">
-        <h5>Visitors</h5>
-        <line-chart 
-          :data="statDate" 
-          :empty="{ empty: 'No data available.' }"/>
+      <div class="d-block black">
+        <b-input-group prepend="Days">
+          <input
+            v-model="days"
+            type="text"
+            class="form-control">
+          <b-input-group-append>
+            <button
+              class="btn btn-primary"
+              @click.prevent="getStat">Update</button>
+          </b-input-group-append>
+        </b-input-group>
       </div>
-      <b-container fluid>
-        <b-row>
-          <b-col cols="6">
-            <h5>Browsers</h5>
-            <pie-chart 
-              :data="statBrowser" 
+      <b-tabs>
+        <b-tab
+          title="Graph"
+          active>
+          <div class="d-block">
+            <h5>Visitors</h5>
+            <line-chart 
+              :data="statDate" 
               :empty="{ empty: 'No data available.' }"/>
-          </b-col>
+          </div>
+          <b-container fluid>
+            <b-row>
+              <b-col cols="6">
+                <h5>Browsers</h5>
+                <pie-chart 
+                  :data="statBrowser" 
+                  :empty="{ empty: 'No data available.' }"/>
+              </b-col>
                     
-          <b-col cols="6">
-            <h5>Platforms</h5>
-            <pie-chart 
-              :data="statOs" 
-              :empty="{ empty: 'No data available.' }"/>
-          </b-col>
-        </b-row>
-      </b-container>
+              <b-col cols="6">
+                <h5>Platforms</h5>
+                <pie-chart 
+                  :data="statOs" 
+                  :empty="{ empty: 'No data available.' }"/>
+              </b-col>
+            </b-row>
+          </b-container>
+        </b-tab>
+
+        <b-tab title="Log">
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Date</th>
+                <th scope="col">Address</th>
+                <th scope="col">Platform</th>
+                <th scope="col">Browser</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="log in statLog"
+                :key="log._id">
+                <td scope="row">{{ log.created_at | formatDate }}</td>
+                <td>{{ log.address }}</td>
+                <td>{{ log.platform }}</td>
+                <td>{{ log.browser }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </b-tab>
+      </b-tabs>
     </b-modal>
   </div>
 </template>
@@ -44,9 +86,11 @@ export default {
   },
   data() {
     return {
+      days: 7,
       statDate: [],
       statBrowser: [],
-      statOs: []
+      statOs: [],
+      statLog: []
     };
   },
   methods: {
@@ -54,11 +98,11 @@ export default {
       this.getStat();
       this.$refs.statModal.show();
     },
-    getStat(days = 7) {
+    getStat() {
       const url =
         this.post == null
-          ? `/pages/${this.page}/stat/${days}`
-          : `/blog/${this.post}/stat/${days}`;
+          ? `/pages/${this.page}/stat/${this.days}`
+          : `/blog/${this.post}/stat/${this.days}`;
 
       this.axios
         .get(`${this.api + url}/number`)
@@ -89,7 +133,25 @@ export default {
         .catch(err => {
           console.log(err);
         });
+
+      this.axios
+        .get(`${this.api + url}/log`)
+        .then(res => {
+          this.statLog = res.data;
+          console.log(res.data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
   }
 };
 </script>
+<style>
+.black {
+  background: rgba(60, 59, 61, 0.2);
+  /* Margin & Padding */
+  padding: 20px;
+  margin: 0 -20px 20px -20px;
+}
+</style>
